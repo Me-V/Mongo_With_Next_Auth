dbb687a7cc4809141b5e40f884000fa350c44425
Me-V <gonemanarmy@gmail.com>
2025-02-05 08:14:27 +0530

    new branch created

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,42 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and the passwords are required ~ Vasu" },
+        { status: 400 }
+      );
+    }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Email is already registered ~ Vasu" },
+        { status: 400 }
+      );
+    }
+
+    await User.create({
+      email,
+      password,
+    });
+
+    return NextResponse.json(
+      { message: "User registered successfully ~ Vasu" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to register the User ~ Vasu" },
+      { status: 500 }
+    );
+  }
+}
